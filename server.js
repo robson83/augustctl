5e49ac5b9063a079c78ad1cf8bd67892bc185ccf
Robson <robson@dxs.com.br>
2016-02-22 16:41:39 -0500

    First working version

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var port = config.port || 3000;
 var app = express();
 app.use(morgan(DEBUG ? 'dev' : 'combined'));
 
+var ret = {'status': -1, 'ret': '', 'msg': ''};
+
 app.get('/api/unlock', function(req, res) {
   var lock = app.get('lock');
   if (!lock) {
@@ -20,14 +22,104 @@ app.get('/api/unlock', function(req, res) {
     return;
   }
 
-  lock.connect().then(function() {
-    return lock.unlock();
-  }).finally(function() {
-    lock.disconnect();
-    // TODO: report errors
-    res.sendStatus(204);
-  });
+  lock.connect().then(function(){
+
+        lock.status().then(function(status){
+
+		ret['ret'] = status;
+
+                if(status == 'locked')
+                {
+                   lock.unlock().then(function() {
+
+			lock.disconnect().then(function() {
+                           ret['msg'] = 'Command completed. Disconnected.';
+			   ret['status'] = 0;
+			   ret['ret'] = 'unlocked';
+			   res.json(ret);
+               		 });
+
+		   });
+                }
+		else
+		{
+			ret['status'] = 1;
+			ret['msg'] = 'Lock is already unlocked';
+			res.json(ret);
+		}
+
+
+        });
+
+   });  
+
 });
+
+
+app.get('/api/lock', function(req, res) {
+  var lock = app.get('lock');
+  if (!lock) {
+    res.sendStatus(503);
+    return;
+  }
+
+  lock.connect().then(function(){
+
+        lock.status().then(function(status){
+
+                ret['ret'] = status;
+
+                if(status == 'unlocked')
+                {
+                   lock.lock().then(function() {
+
+                        lock.disconnect().then(function() {
+                           ret['msg'] = 'Command completed. Disconnected.';
+                           ret['status'] = 0;
+                           ret['ret'] = 'locked';
+                           res.json(ret);
+                         });
+
+                   });
+                }
+                else
+                {
+                        ret['status'] = 1;
+                        ret['msg'] = 'Lock is already locked';
+                        res.json(ret);
+                }
+
+
+        });
+
+   });
+
+});
+
+
+app.get('/api/status', function(req, res){
+
+   var lock = app.get('lock');
+   if(!lock) {
+      res.sendStatus(503);
+      return;
+   }
+
+   lock.connect().then(function(){
+
+	lock.status().then(function(status){
+		ret['ret'] = status;
+		ret['status'] = 0; 
+		lock.disconnect().then(function() {
+      			ret['msg'] = 'Command completed. Disconnected.';
+      			res.json(ret);
+   		});
+	});
+
+   });
+
+});
+
 
 augustctl.scan(config.lockUuid).then(function(peripheral) {
   var lock = new augustctl.Lock(
